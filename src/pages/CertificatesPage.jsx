186a6710aaab27ee65710ba54f5ff3a99c0153fc
lp0408165x-cloud <lp0408165x@gmail.com>
2026-02-14 10:26:44 -0500

    Update CertificatesPage.jsx

--- src/pages/CertificatesPage.jsx
+++ src/pages/CertificatesPage.jsx
@@ -1,4 +1,4 @@
-aimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   ArrowLeft, Award, Calendar, Shield, ShieldCheck, ShieldX, ExternalLink, Copy, Check
