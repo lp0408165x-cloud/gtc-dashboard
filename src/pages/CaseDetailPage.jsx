e7ca68279fa59d81b9afe637c5d8ae5cba594113
lp0408165x-cloud <lp0408165x@gmail.com>
2026-03-08 10:20:07 -0400

    Update CaseDetailPage.jsx

--- src/pages/CaseDetailPage.jsx
+++ src/pages/CaseDetailPage.jsx
@@ -1196,9 +1196,7 @@ const CaseDetailPage = () => {
           {activeTab === 'files' && (
             <div className="space-y-4">
               {/* 证据槽位面板 */}
-              <EvidenceUploadPanel caseId={id} caseType={caseData?.case_type?.replace('-', ' ') || caseData?.case_type} visibleSlots={caseData?.visible_slots ?? []} />
-              {/* 原有的拖拽上传区域保留在下面 */}
-              <div
+              {caseData?.intake_status === 'complete' ? <EvidenceUploadPanel caseId={id} caseType={caseData?.case_type?.replace('-', ' ') || caseData?.case_type} visibleSlots={caseData?.visible_slots ?? []} /> : <IntakeUploadPanel caseId={id} onComplete={() => window.location.reload()} />}
                 onDragOver={handleDragOver}
                 onDragLeave={handleDragLeave}
                 onDrop={handleDrop}
